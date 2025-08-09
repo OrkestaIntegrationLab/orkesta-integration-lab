63fed6ae9433e462e01a963c5885593005db18c2
engel <NGLopz2794@gmail.com>
2025-08-09 16:37:09 -0400

    web (chore) : correccion de estilos (aun con error)

--- WebApps/orkesta-web/src/app/modules/masters/masters-routing.module.ts
+++ WebApps/orkesta-web/src/app/modules/masters/masters-routing.module.ts
@@ -5,7 +5,7 @@ import { LayoutComponent } from '../layout/layout/layout.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'masters',
     component: LayoutComponent,
     children:[
      { path: 'weather-list', component: WeatherForeCastListComponent },
